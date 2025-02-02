372896654d57ebff954c468ded4d73ced66e03b5
Ishant9582 <150500780+Ishant9582@users.noreply.github.com>
2025-02-02 15:16:15 +0530

    ac

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      onwarn(warning, warn) {
-        if (warning.code === 'SOME_WARNING_CODE') return; // Ignore specific warning
-        warn(warning);
-      }
-    }
-  }
-});
+})
